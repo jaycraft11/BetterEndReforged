4fcde75b62fcc887de0129ec3c74d6ea0d70be30
Frank <developer@ambertation.de>
2023-12-19 15:26:53 +0100

    `isDurationEffectTick` -> `shouldApplyEffectTickThisTick`

--- src/main/java/org/betterx/betterend/effects/status/EndVeilEffect.java
+++ src/main/java/org/betterx/betterend/effects/status/EndVeilEffect.java
@@ -10,7 +10,7 @@ public class EndVeilEffect extends MobEffect {
     }
 
     @Override
-    public boolean isDurationEffectTick(int duration, int amplifier) {
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
         return false;
     }
 }
