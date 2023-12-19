5ee9012367d361cf632bc5a06aea7dbd4da5c96a
Frank <developer@ambertation.de>
2023-12-19 21:59:12 +0100

    [Change] Removed old Sodium compatibility Code (#317)

--- src/main/java/org/betterx/betterend/mixin/client/BiomeColorsMixin.java
+++ src/main/java/org/betterx/betterend/mixin/client/BiomeColorsMixin.java
@@ -5,13 +5,10 @@ import org.betterx.betterend.client.ClientOptions;
 import org.betterx.betterend.registry.EndBlocks;
 import org.betterx.ui.ColorUtil;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockPos.MutableBlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
-
-import net.fabricmc.loader.api.FabricLoader;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -31,19 +28,16 @@ public class BiomeColorsMixin {
     private static final int BE_STREAM_COLOR = ColorUtil.color(105, 213, 244);
     @Unique
     private static final Point[] BE_OFFSETS;
-    @Unique
-    private static final boolean BE_HAS_SODIUM;
 
     @Inject(method = "getAverageWaterColor", at = @At("RETURN"), cancellable = true)
     private static void be_getWaterColor(BlockAndTintGetter world, BlockPos pos, CallbackInfoReturnable<Integer> info) {
         if (ClientOptions.useSulfurWaterColor()) {
-            BlockAndTintGetter view = BE_HAS_SODIUM ? Minecraft.getInstance().level : world;
             MutableBlockPos mut = new MutableBlockPos();
             mut.setY(pos.getY());
             for (int i = 0; i < BE_OFFSETS.length; i++) {
                 mut.setX(pos.getX() + BE_OFFSETS[i].x);
                 mut.setZ(pos.getZ() + BE_OFFSETS[i].y);
-                if ((view.getBlockState(mut).is(EndBlocks.BRIMSTONE))) {
+                if ((world.getBlockState(mut).is(EndBlocks.BRIMSTONE))) {
                     info.setReturnValue(i < 4 ? BE_POISON_COLOR : BE_STREAM_COLOR);
                     return;
                 }
@@ -52,8 +46,6 @@ public class BiomeColorsMixin {
     }
 
     static {
-        BE_HAS_SODIUM = FabricLoader.getInstance().isModLoaded("sodium");
-
         int index = 0;
         BE_OFFSETS = new Point[20];
         for (int x = -2; x < 3; x++) {
