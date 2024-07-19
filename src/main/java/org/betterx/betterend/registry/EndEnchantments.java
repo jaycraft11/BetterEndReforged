c0ad9c540583ca031938d320686a9b1798e0b912
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2024-07-19 10:14:56 -0400

    Fix End Veil enchantment

--- src/main/java/org/betterx/betterend/registry/EndEnchantments.java
+++ src/main/java/org/betterx/betterend/registry/EndEnchantments.java
@@ -1,5 +1,9 @@
 package org.betterx.betterend.registry;
 
+import java.util.function.UnaryOperator;
+import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponentType.Builder;
+import net.minecraft.core.registries.BuiltInRegistries;
 import org.betterx.betterend.BetterEnd;
 import org.betterx.wover.enchantment.api.EnchantmentKey;
 import org.betterx.wover.enchantment.api.EnchantmentManager;
@@ -11,11 +15,14 @@ public class EndEnchantments {
     public final static EnchantmentKey END_VEIL = EnchantmentManager.createKey(BetterEnd.C.mk("end_veil"));
 
 
-    public static final DataComponentType<Unit> END_VEIL_STATE = EnchantmentManager.registerDataComponent(
-            BetterEnd.C.mk("end_veil"),
+    public static final DataComponentType<Unit> END_VEIL_STATE = registerEnchantment(
+            BetterEnd.C.mk("end_veil_state").toString(),
             (builder) -> builder.persistent(Unit.CODEC)
     );
 
-    public static void register() {
+    private static DataComponentType<Unit> registerEnchantment(String string, UnaryOperator<Builder<Unit>> unaryOperator) {
+        return Registry.register(BuiltInRegistries.ENCHANTMENT_EFFECT_COMPONENT_TYPE, string, unaryOperator.apply(DataComponentType.builder()).build());
     }
+
+    public static void register() {}
 }
