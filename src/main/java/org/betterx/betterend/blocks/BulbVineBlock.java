e31f09a56a6ea4ca83b5cd7822880ffd0f8abd0f
Frank Bauer <frank.bauer@fau.de>
2024-08-08 00:16:54 +0200

    [change] Rebuilt Loot Tables

--- src/main/java/org/betterx/betterend/blocks/BulbVineBlock.java
+++ src/main/java/org/betterx/betterend/blocks/BulbVineBlock.java
@@ -1,35 +1,33 @@
 package org.betterx.betterend.blocks;
 
 import org.betterx.bclib.blocks.BaseVineBlock;
-import org.betterx.bclib.util.MHelper;
 import org.betterx.betterend.registry.EndBlocks;
 import org.betterx.betterend.registry.EndItems;
+import org.betterx.wover.block.api.BlockProperties;
 import org.betterx.wover.block.api.BlockProperties.TripleShape;
+import org.betterx.wover.loot.api.BlockLootProvider;
+import org.betterx.wover.loot.api.LootLookupProvider;
 
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.BonusLevelTableCondition;
+import net.minecraft.world.level.storage.loot.predicates.ExplosionCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 
-import com.google.common.collect.Lists;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
-public class BulbVineBlock extends BaseVineBlock {
+public class BulbVineBlock extends BaseVineBlock implements BlockLootProvider {
     public BulbVineBlock() {
         super(15, true);
-    }
-
-    @Override
-    public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
-        if (state.getValue(SHAPE) == TripleShape.BOTTOM) {
-            return Lists.newArrayList(new ItemStack(EndItems.GLOWING_BULB));
-        } else if (MHelper.RANDOM.nextInt(8) == 0) {
-            return Lists.newArrayList(new ItemStack(EndBlocks.BULB_VINE_SEED));
-        } else {
-            return Lists.newArrayList();
-        }
     }
 
     @Override
@@ -44,5 +42,52 @@ public class BulbVineBlock extends BaseVineBlock {
                 ? canPlace
                 : canPlace && world.getBlockState(
                         pos.below()).is(this);
+    }
+
+//    @Override
+//    public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
+//        if (state.getValue(SHAPE) == TripleShape.BOTTOM) {
+//            return Lists.newArrayList(new ItemStack(EndItems.GLOWING_BULB));
+//        } else if (MHelper.RANDOM.nextInt(8) == 0) {
+//            return Lists.newArrayList(new ItemStack(EndBlocks.BULB_VINE_SEED));
+//        } else {
+//            return Lists.newArrayList();
+//        }
+//    }
+
+    @Override
+    public LootTable.Builder registerBlockLoot(
+            @NotNull ResourceLocation location,
+            @NotNull LootLookupProvider provider,
+            @NotNull ResourceKey<LootTable> tableKey
+    ) {
+        var bottom = LootItemBlockStatePropertyCondition
+                .hasBlockStateProperties(this)
+                .setProperties(StatePropertiesPredicate.Builder
+                        .properties()
+                        .hasProperty(SHAPE, BlockProperties.TripleShape.BOTTOM));
+
+        return LootTable
+                .lootTable()
+                .withPool(
+                        LootPool
+                                .lootPool()
+                                .setRolls(ConstantValue.exactly(1.0F))
+                                .add(LootItem.lootTableItem(EndItems.GLOWING_BULB.asItem())
+                                             .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))
+                                )
+                                .when(bottom)
+                )
+                .withPool(
+                        LootPool
+                                .lootPool()
+                                .setRolls(ConstantValue.exactly(1.0F))
+                                .add(LootItem.lootTableItem(EndBlocks.BULB_VINE_SEED.asItem())
+                                             .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))
+                                             .when(ExplosionCondition.survivesExplosion())
+                                             .when(BonusLevelTableCondition.bonusLevelFlatChance(provider.fortune(), LootLookupProvider.VANILLA_LEAVES_SAPLING_CHANCES))
+                                )
+                                .when(bottom.invert())
+                );
     }
 }
